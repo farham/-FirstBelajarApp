b2122faf2956f35fd1cfbd5d062b62bb9d6f56e9
farham <lipi.farham@gmail.com>
2019-05-13 22:48:58 +0700

    DElayed_splash

--- app/src/main/java/id/web/farham/farhamharvianto/firstbelajarapp/GetStartedActivity.java
+++ app/src/main/java/id/web/farham/farhamharvianto/firstbelajarapp/GetStartedActivity.java
@@ -2,12 +2,36 @@ package id.web.farham.farhamharvianto.firstbelajarapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class GetStartedActivity extends AppCompatActivity {
+
+    String myEmail = "farham@gmail.com";
+    Boolean kondisi = true;
+    EditText emailAddress;
+    Integer angka_satu = 100, angka_dua = 250, angka_tiga = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_started);
+
+        emailAddress = findViewById(R.id.emailAddress);
+
+        angka_tiga = angka_satu + angka_dua;
+        if(angka_tiga == 10){
+            // Jawaba salah Toas
+            Toast.makeText(getApplicationContext(), "Salah Jumlah", Toast.LENGTH_SHORT).show();
+
+        }else {
+            //emailAddress.setText();
+            Toast.makeText(getApplicationContext(), "Jumlah Benar", Toast.LENGTH_SHORT).show();
+
+        }
+
+
     }
 }
--- app/src/main/java/id/web/farham/farhamharvianto/firstbelajarapp/SplashAct.java
+++ app/src/main/java/id/web/farham/farhamharvianto/firstbelajarapp/SplashAct.java
@@ -2,6 +2,7 @@ package id.web.farham.farhamharvianto.firstbelajarapp;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,26 +11,39 @@ import android.widget.TextView;
 
 public class SplashAct extends AppCompatActivity {
 
-    ImageView applogo;
+    ImageView splashLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        // Mendapati ID pada XML
+        splashLogo = findViewById(R.id.splashLogo);
 
-        // Mendapatkan ID dari XML
-        applogo.findViewById(R.id.applogo);
-
-        // Memberikan Event pada Button
-        applogo.setOnClickListener(new View.OnClickListener() {
+        // Memberikan Event Kepada Button App Logo
+        /*
+        splashLogo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Menuji Halaman Lainya
-                Intent abc = new Intent(SplashAct.this, GetStartedActivity.class);
-                startActivity(abc);
+                // Menuju Halaman Lainn
+                Intent pindah = new Intent(SplashAct.this, GetStartedActivity.class);
+                startActivity(pindah);
                 finish();
             }
-        });
+        });*/
+
+        // Membuat Timer untuk Pindah Activity secara OTomatis
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Melakukan sesuatu setelah 2 Detik
+                Intent pindah = new Intent(SplashAct.this, GetStartedActivity.class);
+                startActivity(pindah);
+                finish();
+            }
+        }, 2000);
+
     }
 }
